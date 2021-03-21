6cb32eebe93857f0a199baf32ca2a61d56170d26
Gilson Alves <gilsonalves6@icloud.com>
2021-03-20 21:07:57 -0300

    teste de credencias

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,6 +14,7 @@ class UserController {
         });
 
         this.formIdUpdate.addEventListener('submit', event => {
+
             event.preventDefault();
             let btn = this.formIdUpdate.querySelector("[type=submit]");
             btn.disabled = true;
