fb73052f8553d22fd9d33594df527e956d5e668f
Gilson Alves <gilsonalves6@icloud.com>
2021-03-22 16:31:44 -0300

    excluindo usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -83,7 +83,7 @@ class UserController {
             <td>${Utils.dateFormate(user.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>`;
 
         this.addEventTr(tr);
@@ -94,6 +94,14 @@ class UserController {
     }
 
     addEventTr(tr) {
+
+        tr.querySelector('.btn-delete').addEventListener('click', e => {
+            if(confirm("Deseja realmente excluir?")){
+                tr.remove();
+                this.updateCount();
+            }
+        });
+
         tr.querySelector('.btn-edit').addEventListener('click', e => {
             let json = JSON.parse(tr.dataset.user);
             this.formIdUpdate.dataset.trIndex = tr.sectionRowIndex;
